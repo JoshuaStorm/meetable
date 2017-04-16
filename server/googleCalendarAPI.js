9e148f2f457f27fdc1ba092b4a8d291d83fca843
JoshuaStorm <jsbecker@princeton.edu>
2017-04-15 20:00:32 -0400

    Hacky way to fix this without cherrypicking

--- server/googleCalendarAPI.js
+++ server/googleCalendarAPI.js
@@ -10,8 +10,8 @@ var auth = new googleAuth(); // used to authentication requests sent by gCalenda
 // Client secrets, etc. from David's (or Casey's?) Google Developer Console project
 
 var oauth2Client = new auth.OAuth2(
-  "411203275095-a0a2bbdtm407ue9km22es9jkn28674nq.apps.googleusercontent.com",
-  "2VDBkNk8dLg18OL1o3XM7Ev4",
+  "940955231388-i5aj301rucberlsfrsje07fj685jm9j7.apps.googleusercontent.com",
+  "hv93jvDPACddBk4sbOV9EJH2",
   "http://localhost:3000/"
 );
 
@@ -46,76 +46,69 @@ Meteor.methods({
   // Get an array of all calendars for the current user
   // return data format: https://developers.google.com/google-apps/calendar/v3/reference/calendarList#resource
   getCalendarList: function() {
-    return wrappedGetCalendarsList({auth: oauth2Client, minAccessRole: "freeBusyReader"});
-  },
-
-  // minTime (momentJS object): earliest time to look for events
-  // maxTime (momentJS object): latest time to look for events
-  getEventListTest: function(minTime, maxTime) {
-    return wrappedGetEventwrappedGetEventList({
-        // The specified calendar
-        // not working for en.usa#holiday@group.v.calendar.google.com
-        // TODO: Don't just do the first ID
-        calendarId: calendarList.items[0].id,
-        // Assumes we are only reading events from now onwards
-        // TODO: Take more than just from now on? Maybe maybe not
-        timeMin: minTime.format("YYYY-MM-DDTHH:mm:ssZ"), //RFC3339
-        timeMax: maxTime.format("YYYY-MM-DDTHH:mm:ssZ"),
-        maxResults: 100,
-        singleEvents: true,
-        orderBy: 'startTime'
-    });
+    return wrappedGetCalendarsList({minAccessRole: "freeBusyReader"});
   },
 
   // Return an array of event dates in the FullCalendar format
-  // SIDE EFFECT: Update database entry for this users events
-  // anonymize (boolean): Whether to return the data anonymized or not
-  getFullCalendarEvents: function(anonymize) {
+  // Updated to use all calendars in the calendarList :)
+  getFullCalendarEvents: function() {
     var calendarList = wrappedGetCalendarsList({minAccessRole: "freeBusyReader"});
-    var gcalEvents = wrappedGetEventList({
-        // The specified calendar
-        // not working for en.usa#holiday@group.v.calendar.google.com
-        // TODO: Don't just do the first ID
-        calendarId: calendarList.items[0].id,
-        // Assumes we are only reading events from now onwards
-        // TODO: Take more than just from now on? Maybe maybe not
-        timeMin: (new Date()).toISOString(),
-        maxResults: 100,
-        singleEvents: true,
-        orderBy: 'startTime'
-    });
+    // Many users have multiple calendars, let's use them all for now
+    // TODO: Include a preference to not include a certain calendar
+    var fullCalEvents = [];
+    var lastWeek = new Date();
+    lastWeek.setDate(lastWeek.getDate() - 7); // This actually works how we want it to!
+    for (var i = 0; i < calendarList.items.length; i++) {
+      // Holiday list creates SERIOUS problems for GoogleAPI (ironically), just avoid at all cost.
+      // This seems to be a known problem, oddly enough. Regexp to the rescue!
+      var holidayRE = new RegExp('.*holiday@group.v.calendar.google.com');
+      if (holidayRE.test(calendarList.items[i].id)) continue;
 
-    anonCalEvents = [];
-    fullCalEvents = [];
-    for (var i = 0; i < gcalEvents.items.length; i++) {
-      var thisGCalEvent = gcalEvents.items[i];
+      var gCalEvents = wrappedGetEventList({
+          // The specified calendar
+          calendarId: calendarList.items[i].id,
+          timeMin: lastWeek.toISOString(),
+          // TODO: Need to decide how to handle this maxResults query... How many should we actually max out?
+          maxResults: 50,
+          singleEvents: true,
+          orderBy: 'startTime'
+      });
+      // Need to skip calendars that Google makes but aren't actually event holding
+      if (gCalEvents === "Not Found")     continue;
+      if (gCalEvents.items === undefined) continue;
 
-      var thisAnonCalEvent = {
-        title: "Unknown",
-        start: thisGCalEvent.start.dateTime,
-        end: thisGCalEvent.end.dateTime,
-        timeZone: thisGCalEvent.start.timeZone
-      };
-      var thisFullCalEvent = {
-        title: thisGCalEvent.summary,
-        start: thisGCalEvent.start.dateTime,
-        end: thisGCalEvent.end.dateTime,
-        timeZone: thisGCalEvent.start.timeZone
-      };
-      anonCalEvents.push(thisAnonCalEvent);
-      fullCalEvents.push(thisFullCalEvent);
-    }
-    // Update this users events in the database with anonymized events
-    Meteor.users.update(this.userId, {
-      $set: {
-        calendarEvents: anonCalEvents
+      for (var j = 0; j < gCalEvents.items.length; j++) {
+        var thisGCalEvent = gCalEvents.items[j];
+        // TODO: Reconsider how to handle full day events, for now just throw them away
+        if (thisGCalEvent.start.dateTime !== undefined) { // If there is no start.dateTime, it's a full day event
+          var thisFullCalEvent = {
+            title: thisGCalEvent.summary,
+            start: thisGCalEvent.start.dateTime,
+            end: thisGCalEvent.end.dateTime,
+            timeZone: thisGCalEvent.start.timeZone
+          };
+          fullCalEvents.push(thisFullCalEvent);
+        }
       }
-    });
-    if (anonymize) return anonCalEvents;
+    }
     return fullCalEvents;
   },
 
-  // DEBUG FUNCTION ONLY THIS SHOULDN'T MAKE IT TO MY PR
+  // Updates datebase with gCalEvents in the fulLCal format for CURRENT USER
+  updateEventsInDB: function() {
+    try {
+      var events = Meteor.call("getFullCalendarEvents");
+      Meteor.users.update(this.userId, {
+        $set: {
+          calendarEvents: events
+        }
+      });
+    } catch(e) {
+      throw "Error in updateEventsInDB" + e;
+    }
+  },
+
+  // Okay, actually I'll leave this function, it's useful for debugging
   printFromDB: function() {
     console.log(Meteor.users.findOne(this.userId).calendarEvents);
   },
@@ -141,8 +134,7 @@ Meteor.methods({
         timeZone: zone
       }
     });
-  },
-
+  }
 });
 
 // Wrapping up async function for Meteor fibers. Confused? See:
@@ -159,36 +151,36 @@ var wrappedGetEventList = Meteor.wrapAsync(gCalendar.events.list);
 
 // Print a list of the next 10 events in the calendar specified by calendarI
 function printEventList(calendarId) {
-        gCalendar.events.list
-        ({
-            // The specified calendar
-            // not working for en.usa#holiday@group.v.calendar.google.com
-            calendarId: calendarId,
+  gCalendar.events.list
+  ({
+    // The specified calendar
+    // not working for en.usa#holiday@group.v.calendar.google.com
+    calendarId: calendarId,
 
-            // Assumes we are only reading events from now onwards
-            timeMin: (new Date()).toISOString(),
-            maxResults: 10,
-            singleEvents: true,
-            orderBy: 'startTime'
-        }, function(err, response)
+    // Assumes we are only reading events from now onwards
+    timeMin: (new Date()).toISOString(),
+    maxResults: 10,
+    singleEvents: true,
+    orderBy: 'startTime'
+  }, function(err, response)
+  {
+    if (err) {
+        console.log('printEventList: The API returned an error: ' + err);
+        return;
+    }
+
+    var events = response.items;
+    if (events.length == 0) {
+        console.log('No upcoming events found.');
+    } else
+    {
+        console.log('Upcoming 10 events:');
+        for (var i = 0; i < events.length; i++)
         {
-            if (err) {
-                console.log('printEventList: The API returned an error: ' + err);
-                return;
-            }
-
-            var events = response.items;
-            if (events.length == 0) {
-                console.log('No upcoming events found.');
-            } else
-            {
-                console.log('Upcoming 10 events:');
-                for (var i = 0; i < events.length; i++)
-                {
-                    var event = events[i];
-                    var start = event.start.dateTime || event.start.date;
-                    console.log('%s - %s', start, event.summary);
-                }
-           }
-        });
+            var event = events[i];
+            var start = event.start.dateTime || event.start.date;
+            console.log('%s - %s', start, event.summary);
+        }
+   }
+  });
 }
