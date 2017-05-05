435d4cc0525b4a96ab94233e7b7187cf4983e305
JoshuaStorm <jsbecker@princeton.edu>
2017-05-04 21:19:12 -0400

    Make events expire. Fix some bugs in deleteMeeting

--- server/main.js
+++ server/main.js
@@ -23,13 +23,13 @@ Meteor.startup(() => {
       check([to, from, subject, text], [String]);
       // Let other method calls from the same client start running,
       // without waiting for the email sending to complete.
-      this.unblock();
-      Email.send({
-        to: to,
-        from: from,
-        subject: subject,
-        text: text
-      });
+      // this.unblock();
+      // Email.send({
+      //   to: to,
+      //   from: from,
+      //   subject: subject,
+      //   text: text
+      // });
     },
   });
 });
